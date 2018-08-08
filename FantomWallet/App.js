581dbc27f2832b5d927dccba463f09bcdba79f91
arshdeep <akaur@innow8apps.com>
2018-08-08 10:49:09 +0530

    - directory

--- FantomWallet/App.js
+++ FantomWallet/App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import CreateWallet from './src/component/CreateWallet/index';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,30 +21,8 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <CreateWallet />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ FantomWallet/src/component/CreateWallet/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class CreateWallet extends Component {
+    render() {
+        return (
+            <View>
+                <View>
+                    <Text>FANTOM</Text>
+                </View>
+                <View>
+                    <Text>Beyond Blockchain</Text>
+                    <Text>The Future of Decentralized Ecosystem</Text>
+                </View>
+                <View>
+                    <Text>Create Wallet</Text>
+                </View>
+                <View>
+                    <Text>Terms of Service</Text>
+                    <Text>Privacy Policy</Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+export default CreateWallet;
