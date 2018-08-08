8534f970f10a355f712a26eebfa96240a245a9d3
arshdeep <akaur@innow8apps.com>
2018-08-08 12:54:58 +0530

    - create Wallet screen

--- FantomWallet/src/component/CreateWallet/index.js
+++ FantomWallet/src/component/CreateWallet/index.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Styles from './styles';
 
 class CreateWallet extends Component {
     render() {
         return (
-            <View>
-                <View>
-                    <Text>FANTOM</Text>
+            <View style={ Styles.mainContainer}>
+                <View style={ Styles.headerContainer}>
+                    <Text style={ Styles.headerText}>FANTOM</Text>
                 </View>
-                <View>
-                    <Text>Beyond Blockchain</Text>
-                    <Text>The Future of Decentralized Ecosystem</Text>
+                <View style={ Styles.subHeaderContainer}>
+                    <Text style={ Styles.subHeaderText1}>Beyond Blockchain</Text>
+                    <Text style={ Styles.subHeaderText2}>The Future of Decentralized Ecosystem</Text>
                 </View>
-                <View>
-                    <Text>Create Wallet</Text>
+                <View style={ Styles.createWallet}>
+                    <Text style={ Styles.createWalletText}>Create Wallet</Text>
                 </View>
-                <View>
-                    <Text>Terms of Service</Text>
-                    <Text>Privacy Policy</Text>
+                <View style={ Styles.footer }>
+                    <Text style={ Styles.footerText1 }>Terms of Service</Text>
+                    <Text style={ Styles.footerText2 }>Privacy Policy</Text>
                 </View>
             </View>
         );
