a4102e6e1d63a18833e8a8bb0597ad4807468a91
ashish <agupta@innow8apps.com>
2018-08-14 11:45:08 +0530

    - format

--- FantomWallet/src/component/addressBook/address/index.js
+++ FantomWallet/src/component/addressBook/address/index.js
@@ -13,11 +13,11 @@ class Address extends Component {
     }
     render() {
         let starIcon = 'star-border';
-        if(this.props.rate){
+        if (this.props.rate) {
             starIcon = 'star'
         }
         return (
-            <View style={{ flexDirection: 'row', marginTop: 10,borderWidth:2,borderColor:'rgb(213,213,213)',padding:5,borderRadius:3 }}>
+            <View style={{ flexDirection: 'row', marginTop: 10, borderWidth: 2, borderColor: 'rgb(213,213,213)', padding: 5, borderRadius: 3 }}>
                 <View style={style.mark}>
                     <Text style={style.markText}>Mark</Text>
                 </View>
@@ -27,8 +27,8 @@ class Address extends Component {
                     <Text>{this.props.line2Text}</Text>
                 </View>
                 <View style={style.icons}>
-                <TouchableOpacity onPress = {() => this.props.rateChange(this.props.id)}><Icon name= {starIcon} size={25} /></TouchableOpacity>
-                    <TouchableOpacity onPress = {() => this.props.delete(this.props.id)}><IconDelete name='trash' size={25} /></TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.props.rateChange(this.props.id)}><Icon name={starIcon} size={25} /></TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.props.delete(this.props.id)}><IconDelete name='trash' size={25} /></TouchableOpacity>
                 </View>
 
             </View>
