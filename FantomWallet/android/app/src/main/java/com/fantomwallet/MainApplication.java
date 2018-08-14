69c50ead05523028ee96e2f1219114515b5b8651
Kapil Bindal <kapilbindal1@gmail.com>
2018-08-14 15:25:47 +0530

    - working on generator

--- FantomWallet/android/app/src/main/java/com/fantomwallet/MainApplication.java
+++ FantomWallet/android/app/src/main/java/com/fantomwallet/MainApplication.java
@@ -3,6 +3,8 @@ package com.fantomwallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSharePackage(),
+            new RNViewShotPackage(),
             new RNCameraPackage(),
             new VectorIconsPackage()
       );
--- FantomWallet/src/component/qr/generator/index.js
+++ FantomWallet/src/component/qr/generator/index.js
@@ -2,17 +2,31 @@
 
 import React, { Component } from 'react'
 import QRCode from 'react-native-qrcode';
+import ViewShot from "react-native-view-shot";
+import Share from 'react-native-share';
+
 
 import {
     StyleSheet,
     View,
-    TextInput
+    TextInput, TouchableOpacity, Text
 } from 'react-native';
 
 export default class QRGenerator extends Component {
   state = {
-    text: 'http://facebook.github.io/react-native/',
+    title: "Fantom Wallet",
+    message: "Hola mundo",
+    text: JSON.stringify({e: '0x87890989812345678789098981234567', a: 5000}),
   };
+
+  onPress() {
+    this.refs.viewShot.capture().then(uri => {
+        console.log("do something with ", uri);
+        Share.open({url: uri})
+    .then((res) => { console.log(res) })
+    .catch((err) => { err && console.log(err); });
+      });
+  }
 
   render() {
     return (
@@ -22,11 +36,13 @@ export default class QRGenerator extends Component {
           onChangeText={(text) => this.setState({text: text})}
           value={this.state.text}
         /> */}
+        <ViewShot ref="viewShot" options={{ format: "jpg" }}>
         <QRCode
           value={this.state.text}
           size={200}
-          bgColor='purple'
           fgColor='white'/>
+          </ViewShot>
+          <TouchableOpacity onPress={() => this.onPress()}><Text>Share</Text></TouchableOpacity>
       </View>
     );
   };
