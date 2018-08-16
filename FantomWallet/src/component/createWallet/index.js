1772540c5a856028f89ae2d53fb2226382266b21
Michael Kong <michaelkong882@gmail.com>
2018-08-16 19:36:32 +0900

    fixed phrases

--- FantomWallet/src/component/createWallet/index.js
+++ FantomWallet/src/component/createWallet/index.js
@@ -15,7 +15,7 @@ class CreateWallet extends Component {
                     <Text style={style.subHeaderText2}>The Future of Decentralized Ecosystem</Text>
                 </View>
                 <TouchableOpacity style={style.createWallet} onPress={() => { this.props.navigation.navigate('CaptionOutput') }} >
-                    <Text style={style.createWalletText}>Create Wallet</Text>
+                    <Text style={style.createWalletText}>Create Wallet test</Text>
                 </TouchableOpacity>
                 <View style={style.footer}>
                     <TouchableOpacity onPress={() => this.props.navigation.navigate('Terms')} >
