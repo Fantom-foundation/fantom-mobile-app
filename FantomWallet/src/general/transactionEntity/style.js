077d9fb74992bd1f4fd840f54f7c1e214baa99f3
ashish <agupta@innow8apps.com>
2018-08-21 17:22:28 +0530

    - sortMenu changes

--- FantomWallet/src/general/transactionEntity/style.js
+++ FantomWallet/src/general/transactionEntity/style.js
@@ -11,13 +11,12 @@ const style = {
         marginBottom: deviceHeight * 0.01,
         // marginBottom: 8,
         // height: 56,
-        height: deviceHeight * 0.07,
+        height: deviceHeight * 0.09,
         backgroundColor: '#fff',
         shadowColor: 'rgb(237,231,246)',
         shadowOffset: { width: 0, height: 10 },
         // shadowOpacity: 0.04
         shadowOpacity: 1
-
     },
     rowOneStyle: {
         flexDirection: 'row',
