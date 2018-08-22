9674f4682a4a02c25f86bba1aa23cd655cf72b98
arshdeep <akaur@innow8apps.com>
2018-08-22 17:01:31 +0530

    - created splash screen for app

--- FantomWallet/src/general/splashScreen/style.js
+++ FantomWallet/src/general/splashScreen/style.js
@@ -1,2 +1,17 @@
+import { Dimensions } from 'react-native';
 
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
 
+const style = {
+    imageBackground: {
+        width: deviceWidth,
+        height: deviceHeight,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+}
+
+export default style;
