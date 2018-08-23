e3c529856fdb70faf47028b41b1a26e4b971d204
ashish <agupta@innow8apps.com>
2018-08-23 17:24:09 +0530

    - minor changes

--- FantomWallet/src/component/CreateWallet/style.js
+++ FantomWallet/src/component/CreateWallet/style.js
@@ -19,25 +19,24 @@ const styles = {
     subHeaderContainer: {
         marginTop:deviceHeight*0.25,
         alignItems: 'center',
-    },
+    },  
     subHeaderText1: {
         color:'white',
         fontWeight: 'bold',
         fontSize: deviceWidth*0.08,
         marginTop: deviceHeight * 0.05,
-        fontFamily:'Segoe-UI'
+        fontFamily:'SegoeUI'
     },
     subHeaderText2: {
         marginTop: deviceHeight * 0.02,
         fontSize: deviceWidth*0.04,
         color:'white',
-        fontFamily:'Segoe-UI'
+        
         
     },
     subHeaderText3: {
         fontSize: deviceWidth*0.04,
-        color:'white',
-        fontFamily:'Segoe-UI'
+        color:'white'
     },
     createWallet: {
         position:'absolute',
@@ -48,14 +47,12 @@ const styles = {
         borderWidth: 1,
         backgroundColor: 'rgb(235,187,17)',
         width: deviceWidth * 0.7,
-        alignSelf:'center',
-        
+        alignSelf:'center'
     },
     createWalletText: {
         fontSize: deviceWidth*0.05,
         marginTop: deviceHeight * 0.02,
         marginBottom: deviceHeight * 0.02,
-        fontFamily:'Segoe-UI-Bold'
     },
     footer: {
         position: 'absolute',
@@ -63,8 +60,7 @@ const styles = {
         flexDirection: 'row',
         width:deviceWidth-40,
         justifyContent: 'space-between',
-        alignSelf:'center',
-        
+        alignSelf:'center'
     },
     footerText1: {
         // marginLeft: deviceWidth * 0.2,
