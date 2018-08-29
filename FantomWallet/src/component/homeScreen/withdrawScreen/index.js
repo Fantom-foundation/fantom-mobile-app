c78642fb9e92f3e2c3bdb556e3b320c2dd05821e
arshdeep <akaur@innow8apps.com>
2018-08-29 17:00:30 +0530

    - keyboard scroll issue on withdraw screen resolved

--- FantomWallet/src/component/homeScreen/withdrawScreen/index.js
+++ FantomWallet/src/component/homeScreen/withdrawScreen/index.js
@@ -73,10 +73,17 @@ export default class WithdrawScreen extends Component {
     }
     this.props.navigation.navigate('SendMoney', { address: address, amount: amount, coin: coin, memo: memo, fees: fees });
   }
+
+
+  onFocus() {
+    let moveBy = deviceHeight <= 568 ? 690 - deviceHeight : 740 - deviceHeight;
+    if (moveBy > 0) {
+      this.scrollView.scrollTo({ x: 0, y: moveBy, animated: true })
+    }
+  }
   onBlur() {
-    if (Platform.OS === 'ios') {
-      return null;
-    } else { return this.scrollView.scrollTo({ x: 0, y: 0, animated: true }) }
+    Keyboard.dismiss();
+    this.scrollView.scrollToEnd()
   }
 
   onScanSuccess(address) {
@@ -98,136 +105,136 @@ export default class WithdrawScreen extends Component {
     const ViewUse = this.state.openSortMenu ? TouchableOpacity : View;
     return (
       <ViewUse activeOpacity={1} style={style.withdrawViewStyle} onPress={() => this.handleClickOnScreen()}>
-        <KeyboardAvoidingView behavior="padding" style={[{ flex: 1}, dynamicStyle]}>
-          <ScrollView ref={(scroll) => this.scrollView = scroll} showsVerticalScrollIndicator={false} >
-            <View style={style.sendContainer}>
-              <Text style={style.sendText}>Send</Text>
-            </View>
+        {/* <KeyboardAvoidingView behavior="padding" style={[{ flex: 1}, dynamicStyle]}> */}
+        <ScrollView ref={(scroll) => this.scrollView = scroll} style={[{ flex: 1 }, dynamicStyle]} showsVerticalScrollIndicator={false} >
+          <View style={style.sendContainer}>
+            <Text style={style.sendText}>Send</Text>
+          </View>
 
 
-            <View style={style.addressContainer}>
-              <Text style={style.addressText}>Address to send</Text>
-              <View style={style.addressTextInputContainer}>
-                <TextInput
-                  onChangeText={(address) => this.setState({ address })}
-                  value={this.state.address}
-                  style={style.addressTextInput}
-                  placeholder='Enter Address'
-                  placeholderTextColor='#a7a7a7'
-                  autoCapitalize='none'
-                />
-                <View style={{ flexDirection: 'row', padding: 5, justifyContent: 'space-around', width: 80 }}>
-                  <TouchableOpacity onPress={this.onScannerPress.bind(this)} style={{ width: 40, height: 28,  alignItems: 'center' }}>
+          <View style={style.addressContainer}>
+            <Text style={style.addressText}>Address to send</Text>
+            <View style={style.addressTextInputContainer}>
+              <TextInput
+                onChangeText={(address) => this.setState({ address })}
+                value={this.state.address}
+                style={style.addressTextInput}
+                placeholder='Enter Address'
+                placeholderTextColor='#a7a7a7'
+                autoCapitalize='none'
+              />
+              <View style={{ flexDirection: 'row', padding: 5, justifyContent: 'space-around', width: 80 }}>
+                <TouchableOpacity onPress={this.onScannerPress.bind(this)} style={{ width: 40, height: 28, alignItems: 'center' }}>
                   <Image source={qrCode} style={{ width: 28, height: 28 }} />
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={this.onContactPress.bind(this)} style={{ width: 40, height: 28, alignItems: 'center' }}>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.onContactPress.bind(this)} style={{ width: 40, height: 28, alignItems: 'center' }}>
                   <Image source={user} style={{ width: 26, height: 26 }} />
-                  </TouchableOpacity>
-                </View>
+                </TouchableOpacity>
               </View>
             </View>
+          </View>
 
-            <View style={style.priceContainer}>
-              <View style={style.priceTextContainer}>
-                <Text style={style.price}>Price</Text>
-                <Text style={style.price}>Current price:12,0000 Won</Text>
-              </View>
-              <View style={style.addressTextInputContainer}>
-                <TextInput
-                  onChangeText={(amount) => this.setState({ amount })}
-                  value={this.state.amount}
-                  style={style.priceTextInput}
-                  placeholder='Enter Amount'
-                  keyboardType='decimal-pad'
-                  placeholderTextColor='#a7a7a7'
-                  autoCapitalize='none'
-                />
-                <TouchableOpacity onPress={() => this.handleSortMenu()} style={{ flexDirection: 'row', padding: 2 }}>
-                  <View style={style.sc}>
-                    <Text>{this.state.val}</Text>
-                  </View>
-                  <Icon name='arrow-drop-down' size={24} color='black' /></TouchableOpacity>
-              </View>
-              <View style={style.availableContainer}>
-                <Text>Available: 12,000000 FTM </Text>
-                <View style={style.allContainer}>
-                  <Text>all</Text>
+          <View style={style.priceContainer}>
+            <View style={style.priceTextContainer}>
+              <Text style={style.price}>Price</Text>
+              <Text style={style.price}>Current price:12,0000 Won</Text>
+            </View>
+            <View style={style.addressTextInputContainer}>
+              <TextInput
+                onChangeText={(amount) => this.setState({ amount })}
+                value={this.state.amount}
+                style={style.priceTextInput}
+                placeholder='Enter Amount'
+                keyboardType='decimal-pad'
+                placeholderTextColor='#a7a7a7'
+                autoCapitalize='none'
+              />
+              <TouchableOpacity onPress={() => this.handleSortMenu()} style={{ flexDirection: 'row', padding: 2 }}>
+                <View style={style.sc}>
+                  <Text>{this.state.val}</Text>
                 </View>
+                <Icon name='arrow-drop-down' size={24} color='black' /></TouchableOpacity>
+            </View>
+            <View style={style.availableContainer}>
+              <Text>Available: 12,000000 FTM </Text>
+              <View style={style.allContainer}>
+                <Text>all</Text>
               </View>
             </View>
+          </View>
 
-            <View style={style.feesContainer}>
-              <Text style={style.feesText}>Fees</Text>
-              <View style={style.addressTextInputContainer}>
-                <TextInput
-                  onChangeText={(fees) => this.setState({ fees })}
-                  value={this.state.fees}
-                  style={style.feesTextInput}
-                  placeholder='Enter Fees'
-                  keyboardType='decimal-pad'
-                  placeholderTextColor='#a7a7a7'
-                  autoCapitalize='none'
-                  onFocus={() => this.scrollView.scrollTo({ x: 0, y: 50, animated: true })}
-                  onBlur={this.onBlur.bind(this)}
-                >
-                </TextInput>
-                <View style={style.ftmTextContainer}>
-                  <Text style={style.ftmText}>0.0000002  FTM</Text>
-                </View>
-              </View>
-              <View style={style.speedContainer}>
-                <View>
-                  <View style={style.slowBar}>
-                  </View>
-                  <Text style={style.slowText}>Slow</Text>
-                </View>
-                <View>
-                  <View style={style.normalBar}>
-                  </View>
-                  <Text style={style.normalText}>Normal</Text>
-                </View>
-                <View>
-                  <View style={style.fastBar}>
-                  </View>
-                  <Text style={style.fastText}>Fast</Text>
-                </View>
+          <View style={style.feesContainer}>
+            <Text style={style.feesText}>Fees</Text>
+            <View style={style.addressTextInputContainer}>
+              <TextInput
+                onChangeText={(fees) => this.setState({ fees })}
+                value={this.state.fees}
+                style={style.feesTextInput}
+                placeholder='Enter Fees'
+                keyboardType='decimal-pad'
+                placeholderTextColor='#a7a7a7'
+                autoCapitalize='none'
+              // onFocus={this.onFocus.bind(this)}
+              // onBlur={this.onBlur.bind(this)}
+              >
+              </TextInput>
+              <View style={style.ftmTextContainer}>
+                <Text style={style.ftmText}>0.0000002  FTM</Text>
               </View>
             </View>
-
-            <View style={style.addressContainer}>
-              <Text style={style.addressText}>Memo</Text>
-              <View style={style.addressTextInputContainer}>
-                <TextInput
-                  onChangeText={(memo) => this.setState({ memo })}
-                  value={this.state.memo}
-                  style={style.addressTextInput}
-                  placeholder='Enter Memo'
-                  placeholderTextColor='#a7a7a7'
-                  onFocus={() => this.scrollView.scrollTo({ x: 0, y: 100, animated: true })}
-                  onBlur={this.onBlur.bind(this)}
-                  autoCapitalize='none'
-                />
+            <View style={style.speedContainer}>
+              <View>
+                <View style={style.slowBar}>
+                </View>
+                <Text style={style.slowText}>Slow</Text>
+              </View>
+              <View>
+                <View style={style.normalBar}>
+                </View>
+                <Text style={style.normalText}>Normal</Text>
+              </View>
+              <View>
+                <View style={style.fastBar}>
+                </View>
+                <Text style={style.fastText}>Fast</Text>
               </View>
             </View>
-            {/* </View> */}
+          </View>
 
-
-            <View style={style.bottomSendContainer}>
-              <Button text='Send'
-                buttonStyle={{ backgroundColor: '#EEBD12', alignSelf: 'center', height: 50 }}
-                textStyle={{ color: '#000', fontWeight: 'normal' }}
-                onPress={this.handleSendMoney.bind(this)} />
+          <View style={style.addressContainer}>
+            <Text style={style.addressText}>Memo</Text>
+            <View style={style.addressTextInputContainer}>
+              <TextInput
+                onChangeText={(memo) => this.setState({ memo })}
+                value={this.state.memo}
+                style={style.addressTextInput}
+                placeholder='Enter Memo'
+                placeholderTextColor='#a7a7a7'
+                onFocus={this.onFocus.bind(this)}
+                onBlur={this.onBlur.bind(this)}
+                autoCapitalize='none'
+              />
             </View>
-            {/* {
+          </View>
+          {/* </View> */}
+
+
+          <View style={style.bottomSendContainer}>
+            <Button text='Send'
+              buttonStyle={{ backgroundColor: '#EEBD12', alignSelf: 'center', height: 50 }}
+              textStyle={{ color: '#000', fontWeight: 'normal' }}
+              onPress={this.handleSendMoney.bind(this)} />
+          </View>
+          {/* {
             this.state.openSortMenu && <View style={{ width: deviceWidth, height: deviceHeight, zIndex: 1, position: 'absolute' }}></View>
           }
           {
             this.state.openSortMenu && <SortMenuCard handleSortMenu={(item) => this.handleSortMenu(item)} data={this.state.data} type={'withDraw'}
               index={this.state.index} />
           } */}
-          </ScrollView>
+        </ScrollView>
 
-        </KeyboardAvoidingView>
+        {/* </KeyboardAvoidingView> */}
         {
           this.state.openSortMenu && <View style={{ width: deviceWidth, height: deviceHeight, zIndex: 1, position: 'absolute' }}></View>
         }
