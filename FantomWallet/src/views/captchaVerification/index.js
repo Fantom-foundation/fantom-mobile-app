61db49bae715feb608b6ff16af1b1e487d39f2ff
Kapil Bindal <kapilbindal1@gmail.com>
2018-09-13 15:31:50 +0530

    - working on linting

--- FantomWallet/src/views/captchaVerification/index.js
+++ FantomWallet/src/views/captchaVerification/index.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Image, Platform, ScrollView, Keyboard, KeyboardAvoidingView, Alert } from 'react-native';
-import { AsyncStorage } from "react-native"
-import ProgressBar from '../../general/progressBar/index';
-import Header from '../../general/header/index';
-import style from './style';
-import Button from '../../general/button/index';
-import InputBox from '../../general/inputBox/index';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Platform,
+  ScrollView,
+  Keyboard,
+  KeyboardAvoidingView,
+} from 'react-native';
 import '../../../global';
-import Web3 from 'web3';
-import EthereumJSWallet from 'ethereumjs-wallet';
 import Hdkey from 'hdkey';
 import EthUtil from 'ethereumjs-util';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { connect } from 'react-redux';
 import * as KeyAction from '../../redux/keys/action';
 // Method of generation taken from: https://medium.com/bitcraft/so-you-want-to-build-an-ethereum-hd-wallet-cb2b7d7e4998;
-import { Dimensions } from 'react-native';
-
-const deviceWidth = Dimensions.get('window').width;
-const deviceHeight = Dimensions.get('window').height;
+import InputBox from '../../general/inputBox/index';
+import Button from '../../general/button/index';
+import style from './style';
+import ProgressBar from '../../general/progressBar/index';
 /**
  * CaptchaVerification: This component is meant for authenticating user with captcha verification,
  *  based secret codes generated in CaptionOutput.
@@ -31,13 +31,32 @@ class CaptchaVerification extends Component {
       phraseFive: '',
       phraseNine: '',
       phraseTwelve: '',
-      'seed': navigation.getParam('seed', 'NO-ID'),
-      'mnemonicWords': navigation.getParam('mnemonicWords', 'NO-ID'),
-      error: ''
+      seed: navigation.getParam('seed', 'NO-ID'),
+      mnemonicWords: navigation.getParam('mnemonicWords', 'NO-ID'),
+      error: '',
     };
     this.walletSetup = this.walletSetup.bind(this);
     this.changePhrase = this.changePhrase.bind(this);
-  };
+  }
+
+  onNavigation() {
+    this.props.navigation.navigate('HomeScreen');
+  }
+
+  onTextFieldFocus() {
+    const scrollValue = Platform.OS === 'ios' ? 80 : 200;
+    setTimeout(() => {
+      this.scrollView.scrollTo({ x: 0, y: scrollValue, animated: true });
+    }, 10);
+  }
+
+  onTextFieldBlur() {
+    Keyboard.dismiss();
+    const scrollValue = Platform.OS === 'ios' ? 0 : 0;
+    setTimeout(() => {
+      this.scrollView.scrollTo({ x: 0, y: scrollValue, animated: true });
+    }, 10);
+  }
 
   /**
    * walletSetup() : This function verifies the user and generates a unique masterPrivateKey for that user.
@@ -49,77 +68,45 @@ class CaptchaVerification extends Component {
     }
     const root = Hdkey.fromMasterSeed(this.state.seed);
     const masterPrivateKey = root.privateKey.toString('hex');
-    const addrNode = root.derive("m/44'/60'/0'/0/0"); //line 1
-    const pubKey = EthUtil.privateToPublic(addrNode._privateKey);
+    const addrNode = root.derive("m/44'/60'/0'/0/0"); // line 1
+    const pubKey = EthUtil.privateToPublic(addrNode._privateKey); // eslint-disable-line
     const addr = EthUtil.publicToAddress(pubKey).toString('hex');
     const address = EthUtil.toChecksumAddress(addr);
-    const key = {
-      'root': root,
-      'masterPrivateKey': masterPrivateKey,
-      'addrNode': addrNode,
-      'pubKey': pubKey,
-      'addr': addr,
-      'address': address
-    };
 
-    const hexPrivateKey = EthUtil.bufferToHex(addrNode._privateKey)
+    const hexPrivateKey = EthUtil.bufferToHex(addrNode._privateKey); // eslint-disable-line
     // const bufferAgain = EthUtil.toBuffer(hexPrivateKey);
     // Save masterPrivateKey to device DO NOT USE IN PRODUCTION
-    // this.saveMasterKey(masterPrivateKey, hexPublicKey);
     this.props.setKeys(masterPrivateKey, address, hexPrivateKey);
     // Save pubKey generation
-    // this.savePublicKey(pubKey);
 
     this.props.navigation.navigate('HomeScreen');
     /*
        If using ethereumjs-wallet instead do after line 1:
        const address = addrNode.getWallet().getChecksumAddressString();
     */
-  };
+  }
 
   /**
    * checkValidation() :  This function is meant to check that captcha entered by user is valid or not.
    *    If invalid then error message is displayed.
    */
   checkValidation() {
-    const phraseFive = this.state.phraseFive;
-    const phraseNine = this.state.phraseNine;
-    const phraseTwelve = this.state.phraseTwelve;
+    const { phraseFive, phraseNine, phraseTwelve } = this.state;
     // check to make sure entered phrases match up.
     if (phraseFive !== this.state.mnemonicWords[4]) {
       this.state.errorMessage = 'Phrase five does not match up.';
       return false;
-    } else if (phraseNine !== this.state.mnemonicWords[8]) {
+    }
+    if (phraseNine !== this.state.mnemonicWords[8]) {
       this.state.errorMessage = 'Phrase nine does not match up.';
       return false;
-    } else if (phraseTwelve !== this.state.mnemonicWords[11]) {
+    }
+    if (phraseTwelve !== this.state.mnemonicWords[11]) {
       this.state.errorMessage = 'Phrase twelve does not match up.';
       return false;
     }
     return true;
-  };
-
-
-  saveMasterKey = async (masterPrivateKey, publicKey) => {
-    try {
-      await AsyncStorage.multiSet([['masterPrivateKey', masterPrivateKey], ['publicKey', publicKey]]);
-    } catch (error) {
-      if (error) {
-        console.log(error);
-      }
-      // Error saving data
-    }
-  };
-  savePublicKey = async (publicKey) => {
-    try {
-      await AsyncStorage.setItem('publicKey', publicKey);
-    } catch (error) {
-      if (error) {
-        console.log(error);
-      }
-      // Error saving data
-    }
-  };
+  }
 
   /**
    * changePhrase()  : on change handler for text fields.
@@ -127,49 +114,25 @@ class CaptchaVerification extends Component {
    * @param {*} phrase : Contains position value of captcha phrase in generated captcha codes.
    */
   changePhrase(text, phrase) {
-    const state = this.state;
+    const { state } = this;
     state[phrase] = text;
     this.setState(state);
-  };
-  getMasterKey() {
-    const masterKeyTest = AsyncStorage.getItem('masterPrivateKey');
-    console.log('getMasterKey');
-    console.log(masterKeyTest);
-  }
-  onLeftIconPress = () => {
-    console.log('onLeftIconPressonLeftIconPress');
-    this.props.navigation.goBack()
-  }
-  onNavigation() {
-    this.props.navigation.navigate('HomeScreen');
-  }
-  onTextFieldFocus() {
-    let scrollValue = (Platform.OS === 'ios') ? 80 : 200
-    setTimeout(() => {
-      this.scrollView.scrollTo({ x: 0, y: scrollValue, animated: true })
-    }, 10);
-  }
-  onTextFieldBlur() {
-    Keyboard.dismiss();
-    let scrollValue = (Platform.OS === 'ios') ? 0 : 0
-    setTimeout(() => {
-      this.scrollView.scrollTo({ x: 0, y: scrollValue, animated: true })
-    }, 10);
   }
 
   render() {
-    let behaviour = (Platform.OS === 'ios') ? 'padding' : null;
+    const behaviour = Platform.OS === 'ios' ? 'padding' : null;
     return (
       <KeyboardAvoidingView behavior={behaviour} style={style.mainContainerStyle}>
-        <ScrollView >
+        <ScrollView>
           <View style={style.mid}>
-
             <View style={style.progressContainer}>
-              <ProgressBar completed='2' remaining='3' />
+              <ProgressBar completed="2" remaining="3" />
             </View>
 
             <View style={style.arrowContainer}>
-              <TouchableOpacity onPress={() => this.props.navigation.goBack()}><Icon name='arrow-back' size={24} color='black' /></TouchableOpacity>
+              <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+                <Icon name="arrow-back" size={24} color="black" />
+              </TouchableOpacity>
             </View>
             <View style={style.headerContainer}>
               <Text style={style.captchaText}>Captcha Verification</Text>
@@ -182,70 +145,77 @@ class CaptchaVerification extends Component {
             <View style={style.textBoxContainer}>
               <View style={style.textBox}>
                 <InputBox
-                  phraseNumber='Enter phrase 5'
+                  phraseNumber="Enter phrase 5"
                   text={this.state.phraseFive}
-                  onChangeText={(text) => this.changePhrase(text, 'phraseFive')}
-
+                  onChangeText={text => this.changePhrase(text, 'phraseFive')}
                 />
-                {(this.state.phraseFive !== '' && this.state.phraseFive !== this.state.mnemonicWords[4]) ? <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-                  <Text style={{ color: 'red' }}>Phrase five does not match up.</Text></View> : null}
+                {this.state.phraseFive !== '' &&
+                this.state.phraseFive !== this.state.mnemonicWords[4] ? (
+                  <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                    <Text style={{ color: 'red' }}>Phrase five does not match up.</Text>
+                  </View>
+                ) : null}
               </View>
               <View style={style.textBox}>
                 <InputBox
-                  phraseNumber='Enter phrase 9'
+                  phraseNumber="Enter phrase 9"
                   text={this.state.phraseNine}
-                  onChangeText={(text) => this.changePhrase(text, 'phraseNine')}
-
+                  onChangeText={text => this.changePhrase(text, 'phraseNine')}
                 />
-                {(this.state.phraseNine !== '' && this.state.phraseNine !== this.state.mnemonicWords[8]) ? <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-                  <Text style={{ color: 'red' }}>Phrase nine does not match up.</Text></View> : null}
+                {this.state.phraseNine !== '' &&
+                this.state.phraseNine !== this.state.mnemonicWords[8] ? (
+                  <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                    <Text style={{ color: 'red' }}>Phrase nine does not match up.</Text>
+                  </View>
+                ) : null}
               </View>
               <View style={style.textBox}>
                 <InputBox
-                  phraseNumber='Enter phrase 12'
+                  phraseNumber="Enter phrase 12"
                   text={this.state.phraseTwelve}
-                  onChangeText={(text) => this.changePhrase(text, 'phraseTwelve')}
-
+                  onChangeText={text => this.changePhrase(text, 'phraseTwelve')}
                 />
-                {(this.state.phraseTwelve !== '' && this.state.phraseTwelve !== this.state.mnemonicWords[11]) ? <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-                  <Text style={{ color: 'red' }}>Phrase twelve does not match up.</Text></View> : null}
+                {this.state.phraseTwelve !== '' &&
+                this.state.phraseTwelve !== this.state.mnemonicWords[11] ? (
+                  <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                    <Text style={{ color: 'red' }}>Phrase twelve does not match up.</Text>
+                  </View>
+                ) : null}
               </View>
             </View>
 
             {/* <View style={{ alignSelf: 'center' }}>
             <Text onPress={this.getMasterKey}>Get Master Key</Text>
           </View> */}
-
-
-
           </View>
         </ScrollView>
         <View style={style.footerStyle}>
-          <Button text='Verify' onPress={this.walletSetup} buttonStyle={{ backgroundColor: 'black' }} />
+          <Button
+            text="Verify"
+            onPress={this.walletSetup}
+            buttonStyle={{ backgroundColor: 'black' }}
+          />
         </View>
       </KeyboardAvoidingView>
     );
   }
 }
 
+const mapStateToProps = () => ({});
 
+const mapDispatchToProps = dispatch => ({
+  setMasterKey: key => {
+    dispatch({ type: KeyAction.MASTER_KEY, key });
+  },
+  setPublicKey: key => {
+    dispatch({ type: KeyAction.PUBLIC_KEY, key });
+  },
+  setKeys: (masterKey, publicKey, privateKey) => {
+    dispatch({ type: KeyAction.MASTER_PUBLIC_PRIVATE_KEY, masterKey, publicKey, privateKey });
+  },
+});
 
-const mapStateToProps = (state) => {
-  return {
-  };
-},
-  mapDispatchToProps = (dispatch) => {
-    return {
-      setMasterKey: (key) => {
-        dispatch({ type: KeyAction.MASTER_KEY, key });
-      },
-      setPublicKey: (key) => {
-        dispatch({ type: KeyAction.PUBLIC_KEY, key });
-      },
-      setKeys: (masterKey, publicKey, privateKey) => {
-        dispatch({ type: KeyAction.MASTER_PUBLIC_PRIVATE_KEY, masterKey, publicKey, privateKey });
-      },
-    };
-  };
-
-export default connect(mapStateToProps, mapDispatchToProps)(CaptchaVerification);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CaptchaVerification);
--- FantomWallet/src/views/captchaVerification/test.js
+++ FantomWallet/src/views/captchaVerification/test.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Image, Platform, ScrollView, Keyboard, KeyboardAvoidingView } from 'react-native';
-import { AsyncStorage } from "react-native"
-import ProgressBar from '../../general/progressBar/index';
-import Header from '../../general/header/index';
-import style from './style';
-import Button from '../../general/button/index';
-import InputBox from '../../general/inputBox/index';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Platform,
+  ScrollView,
+  Keyboard,
+  KeyboardAvoidingView,
+  Dimensions,
+} from 'react-native';
 import '../../../global';
-import Web3 from 'web3';
-import EthereumJSWallet from 'ethereumjs-wallet';
 import Hdkey from 'hdkey';
 import EthUtil from 'ethereumjs-util';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 // Method of generation taken from: https://medium.com/bitcraft/so-you-want-to-build-an-ethereum-hd-wallet-cb2b7d7e4998;
-import { Dimensions } from 'react-native';
+import InputBox from '../../general/inputBox/index';
+import Button from '../../general/button/index';
+import style from './style';
+import ProgressBar from '../../general/progressBar/index';
 
-const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
 class CaptchaVerification extends Component {
   constructor(props) {
@@ -25,31 +28,41 @@ class CaptchaVerification extends Component {
       phraseFive: '',
       phraseNine: '',
       phraseTwelve: '',
-      'seed': navigation.getParam('seed', 'NO-ID'),
-      'mnemonicWords': navigation.getParam('mnemonicWords', 'NO-ID'),
-      error: ''
+      seed: navigation.getParam('seed', 'NO-ID'),
+      mnemonicWords: navigation.getParam('mnemonicWords', 'NO-ID'),
+      error: '',
     };
     this.walletSetup = this.walletSetup.bind(this);
     this.changePhrase = this.changePhrase.bind(this);
-  };
+  }
+
+  onNavigation() {
+    this.props.navigation.navigate('HomeScreen');
+  }
+
+  onTextFieldFocus() {
+    const scrollValue = Platform.OS === 'ios' ? 80 : 200;
+    setTimeout(() => {
+      this.scrollView.scrollTo({ x: 0, y: scrollValue, animated: true });
+    }, 10);
+  }
+
+  onTextFieldBlur() {
+    Keyboard.dismiss();
+    const scrollValue = Platform.OS === 'ios' ? 0 : 0;
+    setTimeout(() => {
+      this.scrollView.scrollTo({ x: 0, y: scrollValue, animated: true });
+    }, 10);
+  }
+
   walletSetup() {
     if (!this.checkValidation()) {
       return;
     }
     const root = Hdkey.fromMasterSeed(this.state.seed);
     const masterPrivateKey = root.privateKey.toString('hex');
-    const addrNode = root.derive("m/44'/60'/0'/0/0"); //line 1
-    const pubKey = EthUtil.privateToPublic(addrNode._privateKey);
-    const addr = EthUtil.publicToAddress(pubKey).toString('hex');
-    const address = EthUtil.toChecksumAddress(addr);
-    const key = {
-      'root': root,
-      'masterPrivateKey': masterPrivateKey,
-      'addrNode': addrNode,
-      'pubKey': pubKey,
-      'addr': addr,
-      'address': address
-    };
+    const addrNode = root.derive("m/44'/60'/0'/0/0"); // line 1
+    const pubKey = EthUtil.privateToPublic(addrNode._privateKey); // eslint-disable-line
     // Save masterPrivateKey to device DO NOT USE IN PRODUCTION
     this.saveMasterKey(masterPrivateKey);
     // Save pubKey generation
@@ -60,92 +73,46 @@ class CaptchaVerification extends Component {
        If using ethereumjs-wallet instead do after line 1:
        const address = addrNode.getWallet().getChecksumAddressString();
     */
-  };
+  }
 
   checkValidation() {
-    const phraseFive = this.state.phraseFive;
-    const phraseNine = this.state.phraseNine;
-    const phraseTwelve = this.state.phraseTwelve;
+    const { phraseFive, phraseNine, phraseTwelve } = this.state;
     // check to make sure entered phrases match up.
     if (phraseFive !== this.state.mnemonicWords[4]) {
       this.state.errorMessage = 'Phrase five does not match up.';
       return false;
-    } else if (phraseNine !== this.state.mnemonicWords[8]) {
+    }
+    if (phraseNine !== this.state.mnemonicWords[8]) {
       this.state.errorMessage = 'Phrase nine does not match up.';
       return false;
-    } else if (phraseTwelve !== this.state.mnemonicWords[11]) {
+    }
+    if (phraseTwelve !== this.state.mnemonicWords[11]) {
       this.state.errorMessage = 'Phrase twelve does not match up.';
       return false;
     }
     return true;
-  };
-  saveMasterKey = async (masterPrivateKey) => {
-    try {
-      await AsyncStorage.setItem('masterPrivateKey', masterPrivateKey);
-    } catch (error) {
-      if (error) {
-        console.log(error);
-      }
-      // Error saving data
-    }
-  };
-  savePublicKey = async (publicKey) => {
-    try {
-      await AsyncStorage.setItem('publicKey', publicKey);
-    } catch (error) {
-      if (error) {
-        console.log(error);
-      }
-      // Error saving data
-    }
-  };
+  }
+
   changePhrase(text, phrase) {
-    const state = this.state;
+    const { state } = this;
     state[phrase] = text;
     this.setState(state);
-  };
-  getMasterKey() {
-    const masterKeyTest = AsyncStorage.getItem('masterPrivateKey');
-    console.log('getMasterKey');
-    console.log(masterKeyTest);
-  }
-  onLeftIconPress = () => {
-    console.log('onLeftIconPressonLeftIconPress');
-    this.props.navigation.goBack()
-  }
-  onNavigation() {
-    this.props.navigation.navigate('HomeScreen');
-  }
-  onTextFieldFocus() {
-    let scrollValue = (Platform.OS === 'ios') ? 80 : 200
-    setTimeout(() => {
-      this.scrollView.scrollTo({ x: 0, y: scrollValue, animated: true })
-    }, 10);
-  }
-  onTextFieldBlur() {
-    Keyboard.dismiss();
-    let scrollValue = (Platform.OS === 'ios') ? 0 : 0
-    setTimeout(() => {
-      this.scrollView.scrollTo({ x: 0, y: scrollValue, animated: true })
-    }, 10);
   }
 
   render() {
     return (
       <KeyboardAvoidingView style={style.mainContainerStyle}>
-
         <View style={style.mid}>
-
           <View style={style.progressContainer}>
-            <ProgressBar completed='2' remaining='3' />
+            <ProgressBar completed="2" remaining="3" />
           </View>
 
           <View style={style.arrowContainer}>
             <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-              <Icon name='arrow-back' size={24} color='black' />
+              <Icon name="arrow-back" size={24} color="black" />
             </TouchableOpacity>
           </View>
-          <ScrollView style={{ height: Platform.OS === 'ios' ? deviceHeight : deviceHeight - 24 }} >
+          <ScrollView style={{ height: Platform.OS === 'ios' ? deviceHeight : deviceHeight - 24 }}>
             <View style={style.headerContainer}>
               <Text style={style.captchaText}>Captcha Verification</Text>
               <View style={style.subHeadContainer}>
@@ -157,55 +124,63 @@ class CaptchaVerification extends Component {
             <View style={style.textBoxContainer}>
               <View style={style.textBox}>
                 <InputBox
-                  phraseNumber='Enter phrase 5'
+                  phraseNumber="Enter phrase 5"
                   text={this.state.phraseFive}
-                  onChangeText={(text) => this.changePhrase(text, 'phraseFive')}
-                // onFocus={() => this.onTextFieldFocus()}
-                // onBlur={() => this.onTextFieldBlur()}
+                  onChangeText={text => this.changePhrase(text, 'phraseFive')}
+                  // onFocus={() => this.onTextFieldFocus()}
+                  // onBlur={() => this.onTextFieldBlur()}
                 />
-                {
-                  (this.state.phraseFive !== '' && this.state.phraseFive !== this.state.mnemonicWords[4]) ? <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-                    <Text style={{ color: 'red' }}>Phrase five does not match up.</Text></View> : null
-                }
+                {this.state.phraseFive !== '' &&
+                this.state.phraseFive !== this.state.mnemonicWords[4] ? (
+                  <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                    <Text style={{ color: 'red' }}>Phrase five does not match up.</Text>
+                  </View>
+                ) : null}
               </View>
               <View style={style.textBox}>
                 <InputBox
-                  phraseNumber='Enter phrase 9'
+                  phraseNumber="Enter phrase 9"
                   text={this.state.phraseNine}
-                  onChangeText={(text) => this.changePhrase(text, 'phraseNine')}
-                // onFocus={() => this.onTextFieldFocus()}
-                // onBlur={() => this.onTextFieldBlur()}
+                  onChangeText={text => this.changePhrase(text, 'phraseNine')}
+                  // onFocus={() => this.onTextFieldFocus()}
+                  // onBlur={() => this.onTextFieldBlur()}
                 />
-                {
-                  (this.state.phraseNine !== '' && this.state.phraseNine !== this.state.mnemonicWords[8]) ? <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-                    <Text style={{ color: 'red' }}>Phrase nine does not match up.</Text></View> : null
-                }
+                {this.state.phraseNine !== '' &&
+                this.state.phraseNine !== this.state.mnemonicWords[8] ? (
+                  <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                    <Text style={{ color: 'red' }}>Phrase nine does not match up.</Text>
+                  </View>
+                ) : null}
               </View>
               <View style={style.textBox}>
                 <InputBox
-                  phraseNumber='Enter phrase 12'
+                  phraseNumber="Enter phrase 12"
                   text={this.state.phraseTwelve}
-                  onChangeText={(text) => this.changePhrase(text, 'phraseTwelve')}
-                // onFocus={() => this.onTextFieldFocus()}
-                // onBlur={() => this.onTextFieldBlur()}
+                  onChangeText={text => this.changePhrase(text, 'phraseTwelve')}
+                  // onFocus={() => this.onTextFieldFocus()}
+                  // onBlur={() => this.onTextFieldBlur()}
                 />
-                {
-                  (this.state.phraseTwelve !== '' && this.state.phraseTwelve !== this.state.mnemonicWords[11]) ? <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-                    <Text style={{ color: 'red' }}>Phrase twelve does not match up.</Text></View> : null
-                }
+                {this.state.phraseTwelve !== '' &&
+                this.state.phraseTwelve !== this.state.mnemonicWords[11] ? (
+                  <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+                    <Text style={{ color: 'red' }}>Phrase twelve does not match up.</Text>
+                  </View>
+                ) : null}
               </View>
             </View>
 
             {/* <View style={{ alignSelf: 'center' }}>
             <Text onPress={this.getMasterKey}>Get Master Key</Text>
           </View> */}
-
           </ScrollView>
           <View style={style.footerStyle}>
-            <Button text='Verify' onPress={this.walletSetup} buttonStyle={{ backgroundColor: 'black' }} />
+            <Button
+              text="Verify"
+              onPress={this.walletSetup}
+              buttonStyle={{ backgroundColor: 'black' }}
+            />
           </View>
         </View>
-
       </KeyboardAvoidingView>
     );
   }
