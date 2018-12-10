2c7d66b20bd2ad8ebb9fca70c68de8a560941598
Kapil Bindal <kmittal@innow8apps.com>
2018-12-10 15:22:09 +0530

    -> UI of AboutApp changed

--- FantomWallet/src/views/homeScreen/settings/aboutApp/index.js
+++ FantomWallet/src/views/homeScreen/settings/aboutApp/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import VersionCheck from 'react-native-version-check';
 import Header from '../../../../general/header/index';
 
-import leftArrowIcon from '../../../../images/arrowLeft_White.png';
 import style from './style';
+import { DEVICE_HEIGHT, DEVICE_WIDTH } from '../../../../common/constants';
 
 class AboutApp extends Component {
   constructor(props) {
@@ -27,36 +27,56 @@ class AboutApp extends Component {
     this.props.navigation.goBack();
   }
 
+  renderDetailsContainer(headimgText, firstLineText, secondLineText) {
+    return (
+      <View style={style.detailsContainer}>
+        <View style={style.detailsHeaderContainer}>
+          <Text style={style.detailHeaderText}>
+            {headimgText} {this.state.version}
+          </Text>
+        </View>
+        <View style={style.innerDetailsContainer}>
+          <Text style={style.detailsInnerText}>{firstLineText}</Text>
+          <Text style={style.detailsInnerText}>{secondLineText}</Text>
+        </View>
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={style.container}>
-        <View>
-          <Header
-            text="About App"
-            leftButtonIcon="chevron-left"
-            leftIconColor="#fff"
-            leftIconSize={22}
-            onLeftIconPress={() => this.onLeftIconPress()}
+        <Header
+          text="About App"
+          leftButtonIcon="chevron-left"
+          leftIconColor="#fff"
+          leftIconSize={22}
+          leftButtonStyle={{ marginLeft: -10 }}
+          onLeftIconPress={() => this.onLeftIconPress()}
+          textStyle={{ fontFamily: 'SFProDisplay-Semibold' }}
+          headerStyle={{
+            backgroundColor: 'rgb(44,52,58)',
+            height: DEVICE_HEIGHT < 810 ? 84 : (106 / 812) * DEVICE_HEIGHT,
+          }}
+        />
+        <View style={style.innerContainerStyle}>
+          {/* About Android */}
+          {this.renderDetailsContainer(
+            'Android version',
+            'Data information: 2018120102022',
+            'Version information 1.0 (latest version)'
+          )}
+          {/* About IOS */}
+          {this.renderDetailsContainer(
+            'ios version',
+            'Data information: 2018120102022',
+            'Version information 1.0 (latest version)'
+          )}
+          <Image
+            style={style.backgroundImageStyle}
+            source={require('../../../../images/BackgroundIcon.png')}
+            resizeMode="contain"
           />
-        </View>
-        <View style={style.body}>
-          <View>
-            <Text style={style.bold}>
-              Android ver
-              {this.state.version}
-            </Text>
-          </View>
-          <View style={style.margin20}>
-            <Text>Data information: 2018120102022</Text>
-            <Text>Version information 1.0 (latest version)</Text>
-          </View>
-          <View>
-            <Text style={{ fontWeight: 'bold', marginTop: 20 }}>iOS ver1.0</Text>
-          </View>
-          <View style={style.margin20}>
-            <Text>Data information: 2018120102022</Text>
-            <Text>Version information 1.0 (latest version)</Text>
-          </View>
         </View>
       </View>
     );
