581b0d9be36591f969355e359da5b126b0190676
Kapil Bindal <kapilbindal1@gmail.com>
2019-12-18 18:58:56 +0530

    - Generate wallet function using private key added

--- FantomWallet/src/redux/keys/saga/generateWallet.js
+++ FantomWallet/src/redux/keys/saga/generateWallet.js
@@ -1,34 +1,58 @@
 // @flow
-import { takeLatest, put } from 'redux-saga/effects';
-import Bip39 from 'react-native-bip39';
-import EthUtil from 'ethereumjs-util';
-import Hdkey from 'hdkey';
+import { takeLatest, put } from "redux-saga/effects";
+import Bip39 from "react-native-bip39";
+import EthUtil from "ethereumjs-util";
+import Hdkey from "hdkey";
 
-import { types, setKeys, setMnemonic } from '../actions';
-import { setDopdownAlert } from '~/redux/notification/actions';
+import { types, setKeys, setMnemonic } from "../actions";
+import { setDopdownAlert } from "~/redux/notification/actions";
 
 type Action = {
   payload: {
     mnemonic: string,
-    cb: () => void,
-  },
+    cb: () => void
+  }
 };
 
 export function* generateWallet({ payload: { mnemonic, cb } }: Action): any {
   try {
     const seed = yield Bip39.mnemonicToSeed(mnemonic); // creates seed buffer
     const root = yield Hdkey.fromMasterSeed(seed);
-    const masterKey = yield root.privateKey.toString('hex');
+    const masterKey = yield root.privateKey.toString("hex");
     const addrNode = root.derive("m/44'/60'/0'/0/0"); // line 1
     const pubKey = yield EthUtil.privateToPublic(addrNode._privateKey);
-    const addr = yield EthUtil.publicToAddress(pubKey).toString('hex');
+    const addr = yield EthUtil.publicToAddress(pubKey).toString("hex");
     const publicKey = yield EthUtil.toChecksumAddress(addr);
     const privateKey = yield EthUtil.bufferToHex(addrNode._privateKey); //eslint-disable-line
-    yield put(setMnemonic({ mnemonic: '' }));
+    console.log(
+      { masterKey, privateKey, publicKey },
+      "{ masterKey, privateKey, publicKey }"
+    );
+    yield put(setMnemonic({ mnemonic: "" }));
     yield put(setKeys({ masterKey, privateKey, publicKey }));
     cb();
   } catch (e) {
-    yield put(setDopdownAlert('error', e.message));
+    yield put(setDopdownAlert("error", e.message));
+  }
+}
+
+export function* generateWalletUsingPrivateKey({
+  payload: { privateKey, cb }
+}: Action): any {
+  try {
+    // const seed = yield Bip39.mnemonicToSeed(mnemonic); // creates seed buffer
+    // const root = yield Hdkey.fromMasterSeed(seed);
+    // const masterKey = yield privateKey.toString("hex");
+    // const addrNode = root.derive("m/44'/60'/0'/0/0"); // line 1
+    // const pubKey = yield EthUtil.privateToPublic(addrNode._privateKey);
+    // const addr = yield EthUtil.publicToAddress(pubKey).toString("hex");
+    // const publicKey = yield EthUtil.toChecksumAddress(addr);
+    // const privateKey = yield EthUtil.bufferToHex(addrNode._privateKey); //eslint-disable-line
+    // yield put(setMnemonic({ mnemonic: "" }));
+    // yield put(setKeys({ masterKey, privateKey, publicKey }));
+    cb();
+  } catch (e) {
+    yield put(setDopdownAlert("error", e.message));
   }
 }
 
