5686c0012d39d32a8f6e6a1d74cdf6bd66481ed4
Kapil Bindal <kapilbindal1@gmail.com>
2019-12-20 19:41:06 +0530

    => commit
    => restore with private Key

--- FantomWallet/src/redux/keys/saga/generateWallet.js
+++ FantomWallet/src/redux/keys/saga/generateWallet.js
@@ -39,15 +39,6 @@ export function* generateWalletUsingPrivateKey({
   payload: { privateKey, publicKey, cb }
 }: Action): any {
   try {
-    // const seed = yield Bip39.mnemonicToSeed(mnemonic); // creates seed buffer
-    // const root = yield Hdkey.fromMasterSeed(seed);
-    // const masterKey = yield privateKey.toString("hex");
-    // const addrNode = root.derive("m/44'/60'/0'/0/0"); // line 1
-    // const pubKey = yield EthUtil.privateToPublic(addrNode._privateKey);
-    // const addr = yield EthUtil.publicToAddress(pubKey).toString("hex");
-    // const publicKey = yield EthUtil.toChecksumAddress(addr);
-    // const privateKey = yield EthUtil.bufferToHex(addrNode._privateKey); //eslint-disable-line
-    // yield put(setMnemonic({ mnemonic: "" }));
     yield put(setKeys({ privateKey, publicKey }));
     cb();
   } catch (e) {
