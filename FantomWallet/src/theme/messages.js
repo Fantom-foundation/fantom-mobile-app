7d69d9d42599c372384b42ef7d14695c21ccc28a
Kapil Bindal <kapilbindal1@gmail.com>
2020-01-20 18:19:42 +0530

    => commit

--- FantomWallet/src/theme/messages.js
+++ FantomWallet/src/theme/messages.js
@@ -2,21 +2,24 @@ import LocalizedStrings from "react-native-localization";
 
 export const Messages = new LocalizedStrings({
   "en-US": {
-    how: "How do you want your egg today?",
-    boiledEgg: "Boiled egg",
-    softBoiledEgg: "Soft-boiled egg",
-    choice: "How to choose the egg"
+    createWallet: "Create a new wallet",
+    availableStake: "Available to stake",
+    currentlyStaking: "Currently staking",
+    stake: "Stake",
+    unstake: "Unstake"
   },
   en: {
-    how: "How do you want your egg today?",
-    boiledEgg: "Boiled egg",
-    softBoiledEgg: "Soft-boiled egg",
-    choice: "How to choose the egg"
+    createWallet: "Create a new wallet",
+    availableStake: "Available to stake",
+    currentlyStaking: "Currently staking",
+    stake: "Stake",
+    unstake: "Unstake"
   },
-  it: {
-    how: "Come vuoi il tuo uovo oggi?",
-    boiledEgg: "Uovo sodo",
-    softBoiledEgg: "Uovo alla coque",
-    choice: "Come scegliere l'uovo"
+  "zh-Hans": {
+    createWallet: "创建一个新的​FTM​钱包",
+    availableStake: "可质押数量",
+    currentlyStaking: "目前质押数量",
+    stake: "权益质押",
+    unstake: "取消权益质押"
   }
 });
